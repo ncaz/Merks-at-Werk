bac7a17f4dd6c0d3efb00082ef1e299470a168f5
Ashwagandhae <julianlianbauer@gmail.com>
2026-09-05 17:00:56 -0400

    update to have bullet damage

--- Player/playerbullet.gd
+++ Player/playerbullet.gd
@@ -4,6 +4,8 @@ const SPEED = 800
 var damage = 1
 
 @onready var axis = Vector2.UP
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -18,10 +20,9 @@ func _on_PlayerBullet_body_entered(body: Node2D) -> void:
 	if body.has_method('enemy_hit'):
 		body.enemy_hit(damage)
 		queue_free()
-		
 
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.has_method("enemy_hit"):
-		area.enemy_hit()
+		area.enemy_hit(damage)
 		queue_free()
--- Station/destructible_part.gd
+++ Station/destructible_part.gd
@@ -19,18 +19,18 @@ func _ready():
 	$Sprite2D.texture = start_texture
 
 
-func enemy_hit():
+func enemy_hit(damage: int):
 	if is_destroyed():
 		return
 
-	current_health -= 1
+	current_health -= damage
 	if is_destroyed():
 		destroy_part()
 
 
 func destroy_part():
 	current_health = 0
-	
+
 	$Sprite2D.texture = destroyed_texture
 
 	$CollisionShape2D.set_deferred("disabled", true)
